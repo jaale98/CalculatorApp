f9a189efa52b43d780dc29fdd14db11372c3e6ed
Jack Alessi <jackalessi7@gmail.com>
2025-05-14 16:44:33 -0400

    basic functions and UI

---
+++ script.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            throw new Error("Unknown operator: " + operator);
+    }
+}
+
+let firstOperand = ""; // first number
+let currentOperator = ""; // operator like + or *
+let secondOperand = ""; // second number
+
